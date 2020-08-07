2e5d4398139d30c12c2e88a48d0020e5cddc6d1c
Dallen Hoyal <dallen.hoyal@gmail.com>
2020-08-07 00:12:18 -0600

    touch test

--- src/input/keyboard.js
+++ src/input/keyboard.js
@@ -8,63 +8,74 @@ module.exports = function (GameboyJS) {
   // Initialize the keyboard listeners and set up the callbacks
   // for button press / release
   Keyboard.prototype.init = function(onPress, onRelease) {
-      this.onPress = onPress;
-      this.onRelease = onRelease;
+    this.onPress = onPress;
+    this.onRelease = onRelease;
 
-      var self = this;
-      document.addEventListener('keydown', function(e) {
-          self.managePress(e.keyCode);
-      });
-      document.addEventListener('keyup', function(e) {
-          self.manageRelease(e.keyCode);
-      });
+    var self = this;
+    document.addEventListener('keydown', function(e) {
+      self.managePress(e.keyCode);
+    });
+    document.addEventListener('keyup', function(e) {
+      self.manageRelease(e.keyCode);
+    });
+    document.addEventListener('touchstart', function(e) {
+      self.manageTouch(e.target.id);
+    })
   }
 
   Keyboard.prototype.managePress = function(keycode) {
-      var key = this.translateKey(keycode);
-      if (key) {
-          this.onPress(key);
-      }
+    var key = this.translateKey(keycode);
+    if (key) {
+      this.onPress(key);
+    }
+  };
+
+  Keyboard.prototype.manageTouch = function(elID) {
+    var key = this.translateKey(elID)
+    if (elID) {
+      this.onPress(key);
+    }
   };
 
   Keyboard.prototype.manageRelease = function(keycode) {
-      var key = this.translateKey(keycode);
-      if (key) {
-          this.onRelease(key);
-      }
+    var key = this.translateKey(keycode);
+    if (key) {
+      this.onRelease(key);
+    }
   };
 
   // Transform a keyboard keycode into a key of the Input.keys object
   Keyboard.prototype.translateKey = function(keycode) {
-      var key = null;
-      switch (keycode) {
-          case 71: // G
-              key = 'A';
-              break;
-          case 66: // B
-              key = 'B';
-              break;
-          case 72: // H
-              key = 'START';
-              break;
-          case 78: // N
-              key = 'SELECT';
-              break;
-          case 37: // left
-              key = 'LEFT';
-              break;
-          case 38: // up
-              key = 'UP';
-              break;
-          case 39: // right
-              key = 'RIGHT';
-              break;
-          case 40: // down
-              key = 'DOWN';
-              break;
-      }
+    var key = null;
+    switch (keycode) {
+      case 71: // G
+        key = 'A';
+        break;
+      case 66: // B
+        key = 'B';
+        break;
+      case 72: // H
+        key = 'START';
+        break;
+      case 78: // N
+        key = 'SELECT';
+        break;
+      case 37: // left
+        key = 'LEFT';
+        break;
+      case 'up':
+      case 38: // up
+        key = 'UP';
+        break;
+      case 39: // right
+        key = 'RIGHT';
+        break;
+      case 40: // down
+        key = 'DOWN';
+        break;
+    }
 
-      return key;
+    return key;
   };
   return Keyboard;
 }
